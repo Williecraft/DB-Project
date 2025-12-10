04d570652b678c08de271ce16473d4266bd4333e
iacon112 <s112056037@mail.nchu.edu.tw>
2025-12-10 21:22:17 +0800

    現在用前端可以create user了;User table裡面有三筆剛剛測試新增的資料，灌CSV檔進去前要先刪掉

--- db_project_backend/main.py
+++ db_project_backend/main.py
@@ -3,13 +3,12 @@ from fastapi.responses import HTMLResponse
 from fastapi.middleware.cors import CORSMiddleware
 import sqlite3
 import random
+from datetime import date
 from typing import List
 
 from db import get_db
 from schemas import *
 
-import random
-import sqlite3
 
 def generate_user_id(db: sqlite3.Connection) -> str:
     prefix = "ur"
@@ -20,7 +19,7 @@ def generate_user_id(db: sqlite3.Connection) -> str:
         user_id = f"{prefix}{number:06d}"
 
         cur = db.execute(
-            "SELECT 1 FROM users WHERE user_id = ?",
+            "SELECT 1 FROM User WHERE user_id = ?",
             (user_id,)
         )
         if cur.fetchone() is None:
@@ -40,20 +39,21 @@ app.add_middleware(
 @app.post("/users", response_model=UserOut, status_code=201)
 def create_user(payload: UserCreate, db: sqlite3.Connection = Depends(get_db)):
     #檢查 email 是否重複
-    cur = db.execute("SELECT 1 FROM users WHERE email = ?", (payload.email,))
+    cur = db.execute("SELECT 1 FROM User WHERE email = ?", (payload.email,))
     if cur.fetchone():
         raise HTTPException(status_code=409, detail="Email already exists")
 
     user_id = generate_user_id(db)
+    join_date = date.today().isoformat()
 
     db.execute(
-        "INSERT INTO users (user_id, name, email, password, age) VALUES (?, ?)",
-        (user_id, payload.name, payload.email, payload.password, payload.age)
+        "INSERT INTO User (user_id, name, email, password, join_date, age) VALUES (?, ?, ?, ?, ?, ?)",
+        (user_id, payload.name, payload.email, payload.password, join_date, payload.age)
     )
     db.commit()
 
     cur = db.execute(
-        "SELECT user_id, name, email, join_date, age FROM users WHERE email = ?",
+        "SELECT user_id, name, email, join_date, age FROM User WHERE email = ?",
         (payload.email,)
     )
     row = cur.fetchone()
@@ -69,7 +69,7 @@ def create_user(payload: UserCreate, db: sqlite3.Connection = Depends(get_db)):
 @app.get("/users/{name}", response_model=UserOut)
 def get_user(name: str, db: sqlite3.Connection = Depends(get_db)):
     cur = db.execute(
-        "SELECT user_id, name, email, join_date, age FROM users WHERE name = ?",
+        "SELECT user_id, name, email, join_date, age FROM User WHERE name = ?",
         (name,)
     )
     row = cur.fetchone()
