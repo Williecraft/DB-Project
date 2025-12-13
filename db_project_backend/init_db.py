c8e5905a0b099965b517f6de98db8428617bbbd1
Williecraft <williechang940118@gmail.com>
2025-12-13 22:12:29 +0800

    剛貼錯了

--- db_project_backend/init_db.py
+++ db_project_backend/init_db.py
@@ -1,54 +1,108 @@
-from fastapi import FastAPI, Depends, HTTPException
-from fastapi.responses import HTMLResponse
+# init_db.py
 import sqlite3
-from typing import List
 
-from db import get_db
-from schemas import *
+DB_PATH = "db_project.db"
 
-app = FastAPI(title="Movie Review API")
+schema_sql = """
+CREATE TABLE Movie(
+    movie_id VARCHAR(15) PRIMARY KEY,
+    director_id INT,
+    title VARCHAR(100) NOT NULL,
+    release_year YEAR NOT NULL,
+    duration INT NOT NULL,
+    language CHAR(2) NOT NULL DEFAULT 'En',
+    country VARCHAR(15) NOT NULL,
+    FOREIGN KEY (director_id) REFERENCES Director(director_id)
+);
 
+CREATE TABLE Director(
+    director_id VARCHAR(15) PRIMARY KEY,
+    name VARCHAR(30) NOT NULL,
+    birth_year YEAR NOT NULL,
+    nationality VARCHAR(10) NOT NULL
+);
 
-@app.post("/users", response_model=UserOut, status_code=201)
-def create_user(payload: UserCreate, db: sqlite3.Connection = Depends(get_db)):
-    #檢查 email 是否重複
-    cur = db.execute("SELECT 1 FROM users WHERE email = ?", (payload.email,))
-    if cur.fetchone():
-        raise HTTPException(status_code=409, detail="Email already exists")
+CREATE TABLE Company(
+    company_id VARCHAR(15) PRIMARY KEY,
+    name VARCHAR(30) NOT NULL UNIQUE,
+    founded_year YEAR NOT NULL,
+    country VARCHAR(10) NOT NULL
+);
 
-    db.execute(
-        "INSERT INTO users (name, email, password, age) VALUES (?, ?)",
-        (payload.name, payload.email, payload.password, payload.age)
-    )
-    db.commit()
+CREATE TABLE Actor(
+    actor_id VARCHAR(15) PRIMARY KEY,
+    name VARCHAR(30) NOT NULL,
+    birth_year YEAR NOT NULL,
+    nationality VARCHAR(10) NOT NULL,
+    gender CHAR(1)
+);
 
-    cur = db.execute(
-        "SELECT user_id, name, email, join_date, age FROM users WHERE email = ?",
-        (payload.email,)
-    )
-    row = cur.fetchone()
-    return UserOut(
-        user_id=row["user_id"],
-        name=row["name"],
-        email=row["email"],
-        join_date=row["join_date"],
-        age=row["age"]
-    )
+CREATE TABLE Role(
+    role_id VARCHAR(15) PRIMARY KEY,
+    name VARCHAR(30) NOT NULL
+);
 
+CREATE TABLE Genre(
+    genre_id VARCHAR(15) PRIMARY KEY,
+    name VARCHAR(15) NOT NULL UNIQUE
+);
 
-@app.get("/users/{name}", response_model=UserOut)
-def get_user(name: str, db: sqlite3.Connection = Depends(get_db)):
-    cur = db.execute(
-        "SELECT user_id, name, email, join_date, age FROM users WHERE name = ?",
-        (name,)
-    )
-    row = cur.fetchone()
-    if row is None:
-        raise HTTPException(status_code=404, detail="User not found")
-    return UserOut(
-        user_id=row["user_id"],
-        name=row["name"],
-        email=row["email"],
-        join_date=row["join_date"],
-        age=row["age"]
-    )
+CREATE TABLE User(
+    user_id VARCHAR(15) PRIMARY KEY,
+    name VARCHAR(10) NOT NULL,
+    email VARCHAR(100) NOT NULL UNIQUE ,
+    password VARCHAR(10) ,
+    join_date DATE NOT NULL ,
+    age TINYINT NOT NULL,
+    CONSTRAINT age_range CHECK (age BETWEEN 0 AND 100)
+);
+
+CREATE TABLE Review(
+    review_id VARCHAR(15) PRIMARY KEY,
+    user_id VARCHAR(15) NOT NULL ,
+    movie_id VARCHAR(15) NOT NULL ,
+    rating TINYINT NOT NULL,
+    comment TEXT NOT NULL,
+    date DATETIME NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES User(user_id),
+    FOREIGN KEY (movie_id) REFERENCES Movie(movie_id)
+);
+
+CREATE TABLE Owns(
+    company_id VARCHAR(15) NOT NULL,
+    movie_id VARCHAR(15) NOT NULL,
+    PRIMARY KEY (company_id, movie_id),
+    FOREIGN KEY (company_id) REFERENCES Company(company_id),
+    FOREIGN KEY (movie_id) REFERENCES Movie(movie_id)
+);
+
+CREATE TABLE RoleInMovie_Played(
+    role_id VARCHAR(15) NOT NULL,
+    actor_id VARCHAR(15) NOT NULL,
+    movie_id VARCHAR(15) NOT NULL,
+    PRIMARY KEY (actor_id, movie_id),
+    FOREIGN KEY (role_id) REFERENCES Role(role_id),
+    FOREIGN KEY (actor_id) REFERENCES Actor(actor_id),
+    FOREIGN KEY (movie_id) REFERENCES Movie(movie_id)
+);
+
+CREATE TABLE MovieGenre(
+    genre_id VARCHAR(15) NOT NULL,
+    movie_id VARCHAR(15) NOT NULL,
+    PRIMARY KEY (genre_id, movie_id),
+    FOREIGN KEY (genre_id) REFERENCES Genre(genre_id),
+    FOREIGN KEY (movie_id) REFERENCES Movie(movie_id)
+);
+"""
+
+def init_db():
+    conn = sqlite3.connect(DB_PATH)
+    try:
+        conn.executescript(schema_sql)
+        conn.commit()
+    finally:
+        conn.close()
+
+if __name__ == "__main__":
+    init_db()
+    print("DB initialized.")
